982fc8b81fd1d87a2cf9d3d5655533598347a3a7
AndreyFA <desenvolvedorCSharp@outlook.com>
2016-09-08 20:48:27 -0300

    Projeto de Testes do Portal MVC.
    Incluso;
    
    - Projeto ControleFinanceiro.UnitTest.
    - Classe UsuarioTests.

---
+++ DevCSharp.ControleFinanceiro/ControleFinanceiro.UI/ControleFinanceiro.UnitTest/UsuarioTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ControleFinanceiro.UnitTest
+{
+    [TestClass]
+    public class UsuarioTests
+    {
+        [TestInitialize]
+        public void UsuarioTestsInitialize()
+        {
+
+        }
+
+        [TestCleanup]
+        public void UsuarioTestsCleanup()
+        {
+
+        }
+    }
+}
